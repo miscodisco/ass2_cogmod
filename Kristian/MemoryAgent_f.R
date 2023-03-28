dbbc26adecb423411290324cf9e8f2d014505866
kristianseverin <201904957@post.au.dk>
2023-03-28 13:03:50 +0200

    param recov for LSWS has been updated to exclude noise.

---
+++ Kristian/MemoryAgent_f.R
@@ -0,0 +1,6 @@
+# Now we create the memory agent whose beta changes based on winning or losing
+MemoryAgent_f <- function(bias, beta, memory, confidence){
+  choice = rbinom(1, 1, inv_logit_scaled(bias + (beta + confidence) * memory))
+  return(choice)
+}
+
